ef030aa1ab0d27322c878e71ca72e5a0f8053c2b
Neelesh Sharma <neeleshsharma@Neeleshs-MacBook-Air.local>
2022-01-29 02:43:27 +0530

    d

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -3,17 +3,15 @@ import React from 'react';
 import { ad } from '../../assets';
 
 export default function HomePage() {
-  return <div style={{minHeight: '100vh', background: "#663399 no-repeat fixed left", backgroundSize: "cover", padding: '35px'}}>
-    <Row>
-        <Col style={{backgroundColor: 'transparent'}} xs={{span:24}} md={{span: 12}} align='left'>
-            <h1 style={{color: 'white'}}>bitsjoy</h1>
-        </Col>
-        <Col style={{backgroundColor: 'transparent'}} xs={{span:0}} md={{span: 12}} align='right'>
-        {/* <h1 style={{color: 'white'}}><Button  style={{fontSize:'35px', height: '100%', color: 'white'}} type="link" size="large">☎️</Button></h1> */}
+  return <div style={{minHeight: '100vh', background: "#264348"}}>
+      
+    <Row style={{position: 'fixed', width: '100%', zIndex: '99', background: "white", top: '0px'}}>
+        <Col style={{backgroundColor: 'transparent'}} xs={{span:24}} md={{span: 24}} align='left'> 
+            <h1 style={{color: '#264348', padding: '20px 35px', paddingBottom :'7px'}}>bitsJoy</h1>
         </Col>
     </Row>
     
-    <Row style={{marginTop: '5vh'}}>
+    <Row style={{paddingTop: '30vh', height: '60vh'}}>
         <Col xs={{span: 3}}></Col>
         <Col xs={{span: 0}} md={{span: 18}}>
             <p style={{fontSize: '30px', fontFamily: 'roboto', color: 'silver'}}>From <span style={{color: 'white'}}>websites</span> and <span style={{color: 'white'}}>portfolios</span> <br/> to mobile <span style={{color: 'white'}}>apps</span>, <br/>we’ll create your <span style={{color: 'white'}}>amazing online presence.</span></p>
@@ -25,10 +23,12 @@ export default function HomePage() {
     </Row>
     <br/> 
 <br/>
-    <Row style={{}}>
+<br/>
+<br/>
+    <Row style={{height: '70vh'}}>
         <Col xs={{span: 24}} align="center">
             
-            <Row style={{verticalAlign: 'center'}}>
+            <Row style={{verticalAlign: 'center', padding: '0px 35px'}}>
             <Col md={{span: 3}} xs={{span:6}}>
                     <hr/>
                 </Col>
@@ -37,7 +37,7 @@ export default function HomePage() {
                     <hr/>
                 </Col>
             </Row> 
-            <div style={{display: 'flex', flexWrap: 'nowrap', overflowX: 'scroll', color: 'white', fontSize: '18px', fontFamily: 'roboto'}}>
+            <div style={{display: 'flex', flexWrap: 'nowrap', overflowX: 'scroll', color: 'white', fontSize: '18px', fontFamily: 'roboto', padding: '0px 35px'}}>
                 <div style={{minWidth: '100px', maxWidth: '100px', border: '0px solid black', marginRight: '37px', textAlign: 'center', marginTop: '20px'}}>
                 <h1 style={{color: 'white'}}><b>R.K.</b></h1>
                     <sub>R.K. Minerals</sub>
@@ -48,36 +48,48 @@ export default function HomePage() {
                 </div>
 
             </div> 
-            <br/> 
             <br/>
-            <div style={{display: 'flex', flexWrap: 'nowrap', overflowX: 'scroll', color: 'white', fontSize: '18px', fontFamily: 'roboto'}}>
+            <Row style={{verticalAlign: 'center', padding: '0px 35px'}}>
+            <Col md={{span: 18}} xs={{span:6}}>
+            <hr/>
+            </Col>
+            <Col md={{span: 3}}  xs={{span:12}} align="center"><h4 style={{color: 'yellow', fontFamily: 'roboto'}}><b>TESTIMONIALS</b></h4></Col>
+            <Col md={{span: 3}} xs={{span:6}}>
+                    <hr/>
+                </Col>
+            </Row> 
+
+            <div style={{padding: '0px 35px', display: 'flex', flexWrap: 'nowrap', overflowX: 'scroll', color: 'white', fontSize: '18px', fontFamily: 'roboto'}}>
                 <span style={{minWidth: '400px', maxWidth: '400px', height: '200px', border: '0px solid black', marginRight: '37px', textAlign: 'left', marginTop: '20px'}}>
                     "I is the Lorem ipsum lorem ipsuem Lorem ipsum lorem ipsuem Lorem  ips " <br/><br/> - Joe
                 </span>
 
                 <span style={{minWidth: '400px', maxWidth: '400px', height: '200px', border: '0px solid black', marginRight: '37px', textAlign: 'left', marginTop: '20px'}}>
                     "Lorem ipsuem ipsuem Lorem  ipsum lorem ipsuem Lorem ipsum lorem ipsuem 
-                    Lorem ipsum lorem ipsuemm Lorem ipsum lorem ipsuem " <br/><br/> - Hoe Garden
+                    Lorem ipsum lorem ipsuemm Lorem ipsum lorem ipsuem " <br/><br/> - Mitchel Garden
                 </span>
 
-                <span style={{minWidth: '400px', maxWidth: '400px', height: '200px', border: '0px solid black', marginRight: '37px', textAlign: 'left', marginTop: '20px'}}>
-                    "Lorem ipsum lorem ipsuem Lorem ipsum lorem ipsuem L " <br/><br/> - Toe 
-                </span>
-
-                <span style={{minWidth: '400px', maxWidth: '400px', height: '200px', border: '0px solid black', marginRight: '37px', textAlign: 'left', marginTop: '20px'}}>
-                    "Lorem ipsum lorem ipsuem Lorem ipsum lor  " <br/><br/> - Tip
-                </span>
-
-                <span style={{minWidth: '400px', maxWidth: '400px', height: '200px', border: '0px solid black', marginRight: '37px', textAlign: 'left', marginTop: '20px'}}>
-                    "Lorem ipsum lorem ipsuem Lorem ipsum lorem ipsuem Lorem  ipsum lorem ipsuem Lorem ipsum lorem ipsuem 
-                    Lorem ipsum loripsum lorem ipsuem " <br/><br/> - Zip 
-                </span>
-
-                <span style={{minWidth: '400px', maxWidth: '400px', height: '200px', border: '0px solid black', marginRight: '37px', textAlign: 'left', marginTop: '20px'}}>
-                    "Lorem ipsum lorem ipsuem Lorem ipsum lorem ipsuem Lorem  ipsum lorem ipsuem Lorem ipsum lorem ipsuem 
-                    Lorem ipsum lorem ipsuem Lorem ipsum lorem ip ipsuem " <br/><br/> - Joe
-                </span>
             </div>
+        </Col>
+    </Row>
+    <Row style={{padding: '20px 35px', background: 'white', fontFamily: 'roboto'}}>
+        <Col xs={{span: 24}} md={{span: 12}} align="left">
+            <h3>Contact details</h3>
+            Phone: +91 9870938860, +91 8126153920 <br/>
+            Email: contact@bitsjoy.com <br/>
+        </Col>
+
+        <Col xs={{span: 0}} md={{span: 12}} align="right">
+            <Button type="text">About us</Button>
+            <Button type="text">Get Quote</Button>
+            <Button type="text">Careers</Button>
+        </Col>
+
+        <Col xs={{span: 24}} md={{span: 0}} align="left">
+            <br/>
+            <Button type="text">About us</Button> 
+            <Button type="text">Get Quote</Button>
+            <Button type="text">Careers</Button>
         </Col>
     </Row>
   </div>;
